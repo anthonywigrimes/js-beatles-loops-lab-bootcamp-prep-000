b754b4ce80bb959519387810d78776aa43c2f555
Anthony Grimes <agrimes@charter.net>
2017-09-15 14:30:20 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(arrMusicians, arrInstruments) {
+    var newArray = [];
+    for (var i = 0; i < arrMusicians.length; i++) {
+        newArray[i] = arrMusicians[i] + ' plays ' + arrInstruments[i];
+    }
+    return newArray;
+};
